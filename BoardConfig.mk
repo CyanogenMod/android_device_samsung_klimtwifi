69b76867480cfcee57a212a6e0ab7a03708c471a
Ethan Chen <intervigil@gmail.com>
2015-08-13 22:16:46 -0700

    i9500: Use newer CM Samsung SLSI HAL repositories
    Change-Id: I1e3e7e7beb91f8a90ac1041ca730836a5be3ad05

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,7 @@ LOCAL_PATH := device/samsung/klimtwifi
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_SLSI_VARIANT := insignal
+TARGET_SLSI_VARIANT := cm
 TARGET_SOC := exynos5420
 
 # Architecture
