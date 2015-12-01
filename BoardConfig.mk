e837c08ef09d0b350aef6c38936eac3a938efe45
Sean hoyt <seanhoyt963@gmail.com>
2015-12-01 07:03:41 -0800

    Add offline charger percentage
    Change-Id: I43da9f5a33c1124d0d997303b51ecc6996b6070b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/klimtwifi
 
 # Charging mode
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := battery
 
