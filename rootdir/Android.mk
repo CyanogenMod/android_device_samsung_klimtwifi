b6c15f39f3b0691de59d4dc9138819124022a1a7
sspencer10 <seanhoyt963@gmail.com>
2015-12-02 23:56:59 -0600

    Move files up to klimt-common
    Change-Id: I0c73bfcbf77988cbc88a5e887d9e81f4ad64b59d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,14 +17,6 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.samsung.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.samsung.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
